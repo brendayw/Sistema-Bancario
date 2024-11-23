43383e22a7b69697e861d469d17e9bbde2f69f83
Brenda Yañez <brendayw97@gmail.com>
2024-11-23 19:52:22 -0300

    primer commit

---
+++ src/main/java/ar/utn/frbb/tup/sistema_bancario/Application.java
@@ -0,0 +1,13 @@
+package ar.utn.frbb.tup.sistema_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
